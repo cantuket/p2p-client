943ba5ceb56975a4d787485e692bd8862a15b864
kyle germaine <kyle@efficacy.io>
2017-09-12 13:41:28 -0500

    first commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,8 +7,8 @@ import { UNAUTH_USER,
          FETCH_LISTINGS,
          FETCH_LISTING 
 } from './types'
-// const ROOT_URL = 'http://104.198.77.63:3090'
-const ROOT_URL = 'http://localhost:3090'
+const ROOT_URL = 'http://104.198.77.63:3090'
+// const ROOT_URL = 'http://localhost:3090'
 
 export function signinUser({email, password}) {
 
