a96060746c5f0c96a5787b35b20989837a062da3
kyle germaine <kyle@efficacy.io>
2017-09-29 21:03:28 -0500

    new IP

--- src/components/listings/items/itemFields.js
+++ src/components/listings/items/itemFields.js
@@ -2,5 +2,7 @@ export default [
 	{ name: 'equipmentType',label:'equipmentType',required:true},
 	{ name: 'make',label:'make'},
 	{ name: 'model',label:'model'},
+	{ name: 'attribute',label:'attribute'},
+	{ name: 'property',label:'property'},
 	{ name: 'condition',label:'condition'},
 ];
